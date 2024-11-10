2fc4c5fc7ea6db24cf2ccffe403f5a70e9733268
Jihoon Song <jihoonsong@users.noreply.github.com>
2024-11-10 19:38:44 +0700

    Allow CORS for prototyping

--- backend/rpc/src/server.rs
+++ backend/rpc/src/server.rs
@@ -1,6 +1,8 @@
+use hyper::Method;
 use jsonrpsee::server::{RpcModule, Server, ServerHandle};
 use rui_backend_client::ClientHandlers;
 use std::net::SocketAddr;
+use tower_http::cors::{Any, CorsLayer};
 
 use crate::{RpcApi, RpcApiServer, RpcError};
 
@@ -31,7 +33,13 @@ where
             .merge(rpc_api.into_rpc())
             .map_err(|e| RpcError::Merge(String::from("rpc_api"), e))?;
 
+        let cors = CorsLayer::new()
+            .allow_methods([Method::POST])
+            .allow_origin(Any)
+            .allow_headers([hyper::header::CONTENT_TYPE]);
+        let middleware = tower::ServiceBuilder::new().layer(cors);
         let server = Server::builder()
+            .set_http_middleware(middleware)
             .build(self.address)
             .await
             .map_err(|e| RpcError::Server(self.address, e))?;
